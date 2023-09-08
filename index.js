da7414e208278c40f12b544dd910b859777f16ba
beibars1396 <beibars1396@me.com>
2023-09-08 23:27:15 +0600

    auth routes impl

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const cors = require("cors");
+const db = require("./config/db");
+const cookieParser = require("cookie-parser");
+const corsOptions = require ("./utils/corsOptions");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+require("dotenv").config();
 
-server.listen(PORT, () => {
+const app = express();
+const PORT = process.env.PORT  || 3000 ;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/../client/public"));
+app.use(cookieParser());
+
+app.use(cors((corsOptions)));
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
---
+++ utils/allowedOrigins.js
@@ -0,0 +1,3 @@
+const allowedOrigins = [ "http://localhost:5173"  , "https://react-frontend-production-50d6.up.railway.app"];
+
+module.exports = allowedOrigins;
