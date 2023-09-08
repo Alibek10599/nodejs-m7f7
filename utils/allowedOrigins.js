5d851d507afc6251a91bef053fb663ed852c3b46
beibars1396 <beibars1396@me.com>
2023-09-09 04:17:26 +0600

    hot fix

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = [ "http://localhost:5173"  , "https://react-frontend-production-50d6.up.railway.app"];
+const allowedOrigins = [ "http://localhost:5173", "https://react-frontend-production-9624.up.railway.app", "https://react-frontend-production-50d6.up.railway.app"];
 
 module.exports = allowedOrigins;
