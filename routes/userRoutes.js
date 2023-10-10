0f435b17d61f23b5ee8524f5fae1c71a7e0a746c
beibars1396 <beibars1396@me.com>
2023-10-10 06:02:26 +0600

    forgotpasswordmail

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,48 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const AdminUserController = require("../controllers/adminUserController");
-// const ClientProductController = require("../controllers/clientProductController");
-// const { resizeProductImages } = require("../middlewares/resizeProductImages");
-// const { uploadProductImages } = require("../middlewares/multerMiddleware");
 const { isAuth, isAdmin } = require("../middlewares/checkAuth");
 
-// router.get("/options", isAuth,isAdmin, AdminProductController.Getoptions);
-
-// router.get("/:id", isAuth,isAdmin, AdminProductController.GetOneProduct);
-
 router.get("/", isAuth, isAdmin, AdminUserController.GetUsers);
-
-// router.post(
-//   "/add",
-//   isAuth,
-//   isAdmin,
-//   uploadProductImages,
-//   resizeProductImages,
-//   AdminProductController.AddProduct
-// );
-// router.patch("/:id/image", isAuth, AdminProductController.DeleteProductImages);
-
-// router.patch(
-//   "/:id",
-//   isAuth,
-//   isAdmin,
-//   uploadProductImages,
-//   resizeProductImages,
-//   AdminProductController.UpdateProduct
-// );
-
-// router.delete("/:id", AdminProductController);
-
-// router.get(
-//   "/client/Products",
-//   ClientProductController.GetClientProducts
-// );
-// router.get("/client/:id", ClientProductController.GetClientOneProduct);
-// router.get(
-//   "/categories/client",
-//   ClientProductController.GetCategories
-// );
-// router.get("/ZiwiShop/search",  ClientProductController.GetSearchProducts);
-// router.get("/relatedProducts/:id", ClientProductController.getRelatedProducts);
 
 module.exports = router;
