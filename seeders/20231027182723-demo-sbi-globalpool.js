90707470666dadc536f947054b7b288071ae0967
Alibek <akhojabekov@gmail.com>
2024-04-19 15:33:04 +0500

    update env

--- seeders/20231027182723-demo-sbi-globalpool.js
+++ seeders/20231027182723-demo-sbi-globalpool.js
@@ -2,23 +2,23 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('GlobalPools', [
       {
         name: 'demoSbiPool',
         login: 'kairhat@gmail.com',
-        password: '2KCj68JCAjHrpb@',
+        password: '',
         token: '',
         isActive: true,
-        apiSecret: 'vMLY98aCqSBh2gWVo9YL5SIvoQV3HFAd',
-        apiKey: 'c6f4c463-3daa-4b1e-95e3-b5eda7890a8d',
+        apiSecret: '',
+        apiKey: '',
         createdAt: new Date(),
         updatedAt: new Date(),
       }
     ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('GlobalPools', null, {});
   }
 };
